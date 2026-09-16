4d573fe77c2b5c1c0328f1d46c5cde3f501b4e07
Nail123488 <2674827836@qq.com>
2026-09-16 08:15:03 +0000

    finish

---
+++ task_b.py
@@ -0,0 +1,10 @@
+a=input("The Slug")
+b=a.lower()
+c=b.spilt()
+d=[]
+for word in c:
+    if word not in ("a", "the"):
+        d.append(word)
+slug="-". join(d)
+slug=slug[:25]
+print(f"Slug={slug}")
