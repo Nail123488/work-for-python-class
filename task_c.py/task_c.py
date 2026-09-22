95767d6f2d9f078b7cd71fb863f7d563b9e8774e
Nail123488 <2674827836@qq.com>
2026-09-22 09:50:47 +0000

    finish

--- task_c.py/task_c.py
+++ task_c.py/task_c.py
@@ -2,21 +2,22 @@ import sys
 from util import read_numbers
 
 numbers = read_numbers()
+
 if len(numbers==0):
     sys.exit("Error: no numbers provided")
-Maximum = max(numbers)
-Minimum = min(numbers)
-Mean = sum(numbers) / len(numbers)
+Max_val = max(numbers)
+Min_val = min(numbers)
+Mean_val = sum(numbers) / len(numbers)
 a = sorted(numbers)
 b = len(a)
 mid = b // 2
 if b%2==1:
-    Median=a[mid]
+    Median_val=a[mid]
 else:
     c=a[mid-1]
     d=a[mid]
-    Median=(c+d)/2
-print(f"Minimum = {Minimum}")
-print(f"Maximum = {Maximum}")
-print(f"Mean    = {Mean}")
-print(f"Median  = {Median}")
+    Median_val=(c+d)/2
+print(f"Minimum = {Min_val}")
+print(f"Maximum = {Max_val}")
+print(f"Mean    = {Mean_val}")
+print(f"Median  = {Median_val}")
