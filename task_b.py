d8af52c049c722f8c688960152ae17a3feb2f792
Nail123488 <2674827836@qq.com>
2026-09-22 09:21:03 +0000

    finish

--- task_b.py
+++ task_b.py
@@ -1,10 +1,10 @@
 a = input()
 b = a.lower()
-c = b.spilt()
-d = []
+c = b.split()
+filtered = []
 for word in c:
-    if word != ("a", "the"):
-        d.append(word)
-slug_str="-". join(d)
-slug=slug_str[:25]
+    if word not in  ("a", "the"):
+        filtered.append(word)
+slug = "-". join(filtered)
+slug = slug[:25]
 print(f"Slug={slug}")
